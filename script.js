36ae8637b4d0bfa747e5da815f46350d9e59ddc4
Cipher95 <h.alobaidan95@gmail.com>
2025-11-15 10:52:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
                /* { p1: 'Cipher', s1: 0, p2: 'libero', s2: 0 },*/
                 { p1: 'Thiago', s1: 2, p2: 'ᴰᵃʳᵏSaidh', s2: 0 },
                /* { p1: 'Cipher', s1: 0, p2: 'Thiago', s2: 0 }, */
-                { p1: 'libero', s1: 2, p2: 'ᴰᵃʳᵏSaidh', s2: 0 }/*,
-                { p1: 'Cipher', s1: 0, p2: 'ᴰᵃʳᵏSaidh', s2: 0 },
+                { p1: 'libero', s1: 2, p2: 'ᴰᵃʳᵏSaidh', s2: 0 },
+                { p1: 'Cipher', s1: 1, p2: 'ᴰᵃʳᵏSaidh', s2: 1 }/*,
                 { p1: 'libero', s1: 0, p2: 'Thiago', s2: 0 } */
             ]
         }, {
