5d2b7adcac1a4188d0f854e7742c4d0143d6bf4a
Cipher95 <h.alobaidan95@gmail.com>
2025-11-17 10:53:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 { p1: 'Thiago', s1: 2, p2: 'ᴰᵃʳᵏSaidh', s2: 0 },
                 { p1: 'Cipher', s1: 0, p2: 'Thiago', s2: 2 },
                 { p1: 'libero', s1: 2, p2: 'ᴰᵃʳᵏSaidh', s2: 0 },
-                { p1: 'Cipher', s1: 1, p2: 'ᴰᵃʳᵏSaidh', s2: 1 }/*,
-                { p1: 'libero', s1: 0, p2: 'Thiago', s2: 0 } */
+                { p1: 'Cipher', s1: 1, p2: 'ᴰᵃʳᵏSaidh', s2: 1 },
+                { p1: 'libero', s1: 0, p2: 'Thiago', s2: 2 }
             ]
         }/*, {
             name: "Group Bravo Standings",
